97c4f9f3fd5ea6b6991467e716b0e27a99939691
maxbons <51479319+maxbons@users.noreply.github.com>
2022-09-20 17:02:05 +0200

    fist setup transfer component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
   <div id="app">
     <h1>Code assignment:</h1>
     <h2>Transfer component</h2>
+
+    <TransferComponent />
   </div>
 </template>
 
 <script>
+import TransferComponent from './components/TransferComponent.vue';
+
 export default {
   name: 'App',
 
-  components: {},
+  components: {
+    TransferComponent,
+  },
 };
 </script>
---
+++ src/components/TransferComponent.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="wrapper">
+  <div class="box">
+    <div v-for="item in items" :key="item.id">
+      <input type="checkbox" :value="item.id">
+        {{ item.name }}
+      </input>
+    </div>
+  </div>
+
+  <div class="buttons-wrapper">
+    <button type="button"> -> </button>
+    <button type="button"> <- </button>
+  </div>
+
+  <div class="box">
+    <div v-for="item in transferedItems" :key="item.id">
+      <input type="checkbox" :value="item.id">
+        {{ item.name }}
+      </input>
+    </div>
+  </div> 
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'TransferComponent',
+
+  data() {
+    return {
+      transferedItems: [],
+      items: [
+        {
+          id: 0,
+          name: 'Max',
+        },
+        {
+          id: 1,
+          name: 'Luuk',
+        },
+        {
+          id: 2,
+          name: 'Shaldi',
+        },
+        {
+          id: 3,
+          name: 'Arie',
+        },
+        {
+          id: 4,
+          name: 'Tobias',
+        },
+        {
+          id: 5,
+          name: 'Sjors',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.wrapper {
+  display: flex;
+}
+
+.box {
+  border: 1px solid black;
+  min-width: 150px;
+  /* margin-right: 2rem; */
+}
+
+.buttons-wrapper {
+  
+}
+</style>
